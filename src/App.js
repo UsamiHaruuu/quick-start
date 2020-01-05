337bab47ef1551d3ec677e25eb0d9a7d8c62b2ac
UsamiHaruuu <lanborui@Outlook.com>
2020-01-05 00:21:37 -0600

    slice 5

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
-// const schedule = {
-//   "title": "CS Courses for 2018-2019",
-//   "courses": [
-//     {
-//       "id": "F101",
-//       "title": "Computer Science: Concepts, Philosophy, and Connections",
-//       "meets": "MWF 11:00-11:50"
-//     },
-//     {
-//       "id": "F110",
-//       "title": "Intro Programming for non-majors",
-//       "meets": "MWF 10:00-10:50"
-//     },
-//     {
-//       "id": "F111",
-//       "title": "Fundamentals of Computer Programming I",
-//       "meets": "MWF 13:00-13:50"
-//     },
-//     {
-//       "id": "F211",
-//       "title": "Fundamentals of Computer Programming II",
-//       "meets": "TuTh 12:30-13:50"
-//     }
-//   ]
-// };
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
 
 const Banner = p => (
   <Title>{p.title || '[loading...]'}</Title>
@@ -41,26 +17,110 @@ const getCourseNumber = p => (
   p.id.slice(1, 4)
 );
 
-const Course = (prop) => (
+const Course = ({course, state}) => (
   <div>
-    <Button>
-      {getCourseTerm(prop.course)} CS {getCourseNumber(prop.course)}: {prop.course.title}
+    <Button onClick = {() => state.toggle(course)}
+            disabled={ hasConflict(course, state.selected) }
+            >
+      {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
     </Button>
   </div>
 );
 
-const CourseList = (prop) => (
-  <div>
-    <Button.Group>
-      {prop.courses.map(course => <Course key={course.title} course={course} />)}
-    </Button.Group>
-  </div>
-);
+const buttonColor = selected =>(
+  selected ? 'success' : null
+)
+const CourseList = (prop) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = prop.courses.filter(course => term === getCourseTerm(course));
+  return (
+    <React.Fragment>
+      <TermSelector state = {{term, setTerm}}/>
+      <Button.Group>
+        {termCourses.map(course => 
+        <Course key={course.id}
+         course={course} 
+         state = {{selected, toggle}} 
+         />)}
+      </Button.Group>
+    </React.Fragment>
+  )
+};
 //key should be unique
 
 const Scoreboard = () => {
   const [score, setScore] = useState(0);
 }
+
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (prop) => {
+    setSelected(selected.includes(prop) ? selected.filter(y => y !== prop) : [prop].concat(selected))
+  };
+  return [ selected, toggle ];
+};
+
+const hasConflict = (course, selected) =>(
+  selected.some(selection => courseConflict (course, selection))
+)
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const TermSelector = ({state}) => {
+  return (
+    <Button.Group has Addons>
+      {
+        Object.values(terms)
+          .map(value =>
+           <Button key={value} 
+                   color = {buttonColor(value===state.term)}
+                   onClick = { () => state.setTerm(value) }
+            >{value}
+            </Button>)
+      }
+    </Button.Group>
+  )
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
@@ -71,7 +131,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
       //schedule => json now
     }
     fetchSchedule();
