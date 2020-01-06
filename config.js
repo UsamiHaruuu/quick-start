45f4a787a4642e0d4ec7e11f408f97290fc7571b
UsamiHaruuu <lanborui@Outlook.com>
2020-01-05 21:36:04 -0600

    stage 6-others

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFObHvrzBwAFBm40_tK4vioHPfcAnxAH4",
+    authDomain: "quick-start-31bbc.firebaseapp.com",
+    databaseURL: "https://quick-start-31bbc.firebaseio.com",
+    projectId: "quick-start-31bbc",
+    storageBucket: "quick-start-31bbc.appspot.com",
+    messagingSenderId: "561010780794",
+    appId: "1:561010780794:web:f580ca6ef6578bb09869c3",
+    measurementId: "G-0JS6EZZZVJ"
+  };
+  module.exports = firebaseConfig;
