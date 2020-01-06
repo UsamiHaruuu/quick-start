cb952e8ed8cb1cb5e283c888bc05d6eb4b8edde8
UsamiHaruuu <lanborui@Outlook.com>
2020-01-05 23:36:04 -0600

    slice 7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title } from 'rbx';
+import { Button, Container, Title, Message } from 'rbx';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBFObHvrzBwAFBm40_tK4vioHPfcAnxAH4",
@@ -20,8 +23,39 @@ const db = firebase.database().ref();
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
 const days = ['M', 'Tu', 'W', 'Th', 'F'];
 
-const Banner = p => (
-  <Title>{p.title || '[loading...]'}</Title>
+const Welcome = ({ user }) => (
+  <Message color="info">
+    <Message.Header>
+      Welcome, {user.displayName}
+      <Button primary onClick={() => firebase.auth().signOut()}>
+        Log out
+      </Button>
+    </Message.Header>
+  </Message>
+);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};  
+
+const SignIn = () => (
+  <StyledFirebaseAuth
+    uiConfig={uiConfig}
+    firebaseAuth={firebase.auth()}
+  />
+);
+
+const Banner = ({ user, title }) => (
+  <React.Fragment>
+    { user ? <Welcome user={ user } /> : <SignIn /> }
+    <Title id = "head">{ title || '[loading...]' }</Title>
+  </React.Fragment>
 );
 
 const getCourseTerm = p => (
@@ -32,13 +66,13 @@ const getCourseNumber = p => (
   p.id.slice(1, 4)
 );
 
-const Course = ({course, state}) => (
+const Course = ({ course, state, user }) => (
   <div>
-    <Button onClick = {() => state.toggle(course)}
-            disabled={ hasConflict(course, state.selected) }
-            color = {buttonColor(state.selected.includes(course))}
-            onDoubleClick = { () => moveCourse(course) }
-            >
+    <Button onClick={() => state.toggle(course)}
+      disabled={hasConflict(course, state.selected)}
+      color={buttonColor(state.selected.includes(course))}
+      onDoubleClick={user?()=>moveCourse(course):null}
+    >
       {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
     </Button>
   </div>
@@ -46,33 +80,34 @@ const Course = ({course, state}) => (
 
 const moveCourse = (course) => {
   const meets = prompt('Enter new data in this format: ', course.meets);
-  if(!meets)return;
-  const {days} = timeParts(meets);
-  if(days) saveCourse(course,meets);
+  if (!meets) return;
+  const { days } = timeParts(meets);
+  if (days) saveCourse(course, meets);
   else moveCourse(course);
 }
 
 const saveCourse = (course, meets) => {
-  db.child('courses').child(course.id).update({meets})
+  db.child('courses').child(course.id).update({ meets })
     .catch(error => alert(error));
 }
 
-const buttonColor = selected =>(
+const buttonColor = selected => (
   selected ? 'success' : null
 )
-const CourseList = (prop) => {
+const CourseList = ({courses,user}) => {
   const [term, setTerm] = useState('Fall');
   const [selected, toggle] = useSelection();
-  const termCourses = prop.courses.filter(course => term === getCourseTerm(course));
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
   return (
     <React.Fragment>
-      <TermSelector state = {{term, setTerm}}/>
+      <TermSelector state={{ term, setTerm }} />
       <Button.Group>
-        {termCourses.map(course => 
-        <Course key={course.id}
-         course={course} 
-         state = {{selected, toggle}} 
-         />)}
+        {termCourses.map(course =>
+          <Course key={course.id}
+            course={course}
+            state={{ selected, toggle }}
+            user={user}
+          />)}
       </Button.Group>
     </React.Fragment>
   )
@@ -88,11 +123,11 @@ const useSelection = () => {
   const toggle = (prop) => {
     setSelected(selected.includes(prop) ? selected.filter(y => y !== prop) : [prop].concat(selected))
   };
-  return [ selected, toggle ];
+  return [selected, toggle];
 };
 
-const hasConflict = (course, selected) =>(
-  selected.some(selection => courseConflict (course, selection))
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
 )
 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
@@ -108,17 +143,7 @@ const timeParts = meets => {
   };
 };
 
-const addCourseTimes = course => ({
-  ...course,
-  ...timeParts(course.meets)
-});
-
-const addScheduleTimes = schedule => ({
-  title: schedule.title,
-  courses: schedule.courses.map(addCourseTimes)
-});
-
-const daysOverlap = (days1, days2) => ( 
+const daysOverlap = (days1, days2) => (
   days.some(day => days1.includes(day) && days2.includes(day))
 );
 
@@ -136,15 +161,15 @@ const courseConflict = (course1, course2) => (
   && timeConflict(course1, course2)
 );
 
-const TermSelector = ({state}) => {
+const TermSelector = ({ state }) => {
   return (
     <Button.Group has Addons>
       {
         Object.values(terms)
           .map(value =>
-           <Button key={value} 
-                   color = {buttonColor(value===state.term)}
-                   onClick = { () => state.setTerm(value) }
+            <Button key={value}
+              color={buttonColor(value === state.term)}
+              onClick={() => state.setTerm(value)}
             >{value}
             </Button>)
       }
@@ -152,24 +177,34 @@ const TermSelector = ({state}) => {
   )
 };
 
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: Object.values(schedule.courses).map(addCourseTimes)
+});
+
 const App = () => {
+  const [user, setUser] = useState(null);
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-  const addScheduleTimes = (schedule) =>({
-    title:schedule.title,
-    courses: Object.values(schedule.courses).map(addCourseTimes)
-  })
+
   useEffect(() => {
     const handleData = snap => {
       if (snap.val()) setSchedule(addScheduleTimes(snap.val()));
     }
-    db.on('value',handleData, error => alert(error));
+    db.on('value', handleData, error => alert(error));
     return () => { db.off('value', handleData) };
   }, [])
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
   return (
     <Container>
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+      <Banner title={schedule.title} user = {user} />
+      <CourseList courses={schedule.courses} user = {user}/>
     </Container>
   )
 };
